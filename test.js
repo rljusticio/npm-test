ab017b65930f3150ed2e07df984586ff38546b80
rljusticio <j.donnell.walters@gmail.com>
2016-02-29 18:23:13 -0500

    saved module to dependencies, created test.js, updated package.json, notes continue as usual

---
+++ test.js
@@ -0,0 +1,15 @@
+/*
+
+First, create a file called `test.js`.  It doesn't have to do anything,
+really.  (This is npm class, not testing class.)  But it has to exit
+without throwing an error, or else the test fails.
+
+Then, edit your `package.json` file to make your scripts section look like
+this instead:
+
+  "scripts": {
+    "test": "node test.js"
+  }
+
+*/
+console.log('Yup, this is the test. It probably passed or something.');
